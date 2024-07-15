de1872cf41af686033062dea7264e278dc90008b
Ken Van den Enden <ken@enden.be>
2024-07-15 10:35:56 +0200

    Create web3 provider.

---
+++ web/src/web3/provider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { WagmiProvider, createConfig, http } from "wagmi";
+import { base, baseSepolia } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export const config = createConfig({
+  chains: [base, baseSepolia],
+  transports: {
+    [base.id]: http(),
+    [baseSepolia.id]: http(),
+  },
+});
+
+const queryClient = new QueryClient();
+
+export default function Web3Provider({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  );
+}
