d128a7bad39384647fbcaba3c6a9b6f349ba3ec0
Ken Van den Enden <ken@enden.be>
2024-07-19 10:54:14 +0200

    Add onchain renderer base.

---
+++ contracts/src/OnchainRenderer.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {Metadata, DisplayType} from "shipyard-core/onchain/Metadata.sol";
+import {json} from "shipyard-core/onchain/json.sol";
+import {Solarray} from "solarray/Solarray.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+
+import {ITokenRenderer} from "./interfaces/ITokenRenderer.sol";
+
+abstract contract OnchainRenderer is ITokenRenderer {
+    using Strings for uint256;
+
+    address private _token;
+
+    string private _name;
+    string private _description;
+
+    constructor(address token_, string memory name_, string memory description_) {
+        _token = token_;
+        _name = name_;
+        _description = description_;
+    }
+
+    function token() public view virtual returns (address) {
+        return _token;
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual returns (string memory) {
+        return Metadata.base64JsonDataURI(metadata(tokenId));
+    }
+
+    function metadata(uint256 tokenId) public view virtual returns (string memory) {
+        return json.objectOf(
+            Solarray.strings(
+                json.property("name", _getName(tokenId)),
+                json.property("description", _getDescription(tokenId)),
+                json.property("image", _getImage(tokenId)),
+                json.rawProperty("attributes", json.arrayOf(_getAttributes(tokenId)))
+            )
+        );
+    }
+
+    function _getName(uint256 tokenId) internal view virtual returns (string memory) {
+        return string.concat(_name, " #", tokenId.toString());
+    }
+
+    function _getDescription(uint256 /* tokenId */ ) internal view virtual returns (string memory) {
+        return _description;
+    }
+
+    function _getImage(uint256 tokenId) internal view virtual returns (string memory);
+
+    function _getAttributes(uint256 /* tokenId */ ) internal view virtual returns (string[] memory);
+
+    function _traitMetadata(string memory name, string memory value) internal pure virtual returns (string memory) {
+        return json.objectOf(Solarray.strings(json.property("trait_type", name), json.property("value", value)));
+    }
+
+    function _traitMetadata(string memory name, string memory value, DisplayType displayType)
+        internal
+        pure
+        virtual
+        returns (string memory)
+    {
+        return json.objectOf(
+            Solarray.strings(
+                json.property("display_type", Metadata.toString(displayType)),
+                json.property("trait_type", name),
+                json.property("value", value)
+            )
+        );
+    }
+
+    function _traitMetadata(string memory name, uint256 value) internal pure virtual returns (string memory) {
+        return json.objectOf(
+            Solarray.strings(json.property("trait_type", name), json.rawProperty("value", value.toString()))
+        );
+    }
+
+    function _traitMetadata(string memory name, uint256 value, DisplayType displayType)
+        internal
+        pure
+        virtual
+        returns (string memory)
+    {
+        return json.objectOf(
+            Solarray.strings(
+                json.property("display_type", Metadata.toString(displayType)),
+                json.property("trait_type", name),
+                json.rawProperty("value", value.toString())
+            )
+        );
+    }
+}
