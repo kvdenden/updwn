22a386825b5873ce6a71c9acf8a35b322910aca2
Jan Van den Enden <jan@arcadia.finance>
2024-08-15 16:10:28 +0200

    add icons to info items

--- web/src/components/InfoSection.tsx
+++ web/src/components/InfoSection.tsx
@@ -1,45 +1,57 @@
 import { Skeleton } from "@/components/ui/skeleton";
+import { IoTicketOutline } from "react-icons/io5";
+import { FaMagnifyingGlassChart, FaGift, FaPuzzlePiece } from "react-icons/fa6";
+import { MdEventRepeat } from "react-icons/md";
+import { GiCrystalBall, GiThreeFriends, GiLockedChest } from "react-icons/gi";
 
 const items = [
   {
     title: "Get your ticket",
     description:
       "Mint your unique pattern of ETH price movements. Patterns have 12 slots that indicate upwards or downwards price changes.",
+    icon: <IoTicketOutline className="h-[1.8rem] w-[1.8rem]" />,
   },
   {
     title: "Tracking ETH Together",
     description:
       "Snapshots of ETH's price movement (sourced from Chainlink) are stored onchain daily. Anyone can trigger a snapshot, when 24 hours have passed since the previous one.",
+    icon: <FaMagnifyingGlassChart className="h-[1.8rem] w-[1.8rem]" />,
   },
   {
     title: "Match the pattern",
     description:
       "To win, you need to hold the ticket whose pattern perfectly matches the actual ETH price movements for the past 12 days.",
+    icon: <FaPuzzlePiece className="h-[1.8rem] w-[1.8rem]" />,
   },
   {
     title: "The daily prize",
     description:
       "100% of the funds are used to provide liquidity to DeFi protocols, generating yield. Daily prizes are based on this yield, creating a sustainable reward system.",
+    icon: <FaGift className="h-[1.8rem] w-[1.8rem]" />,
   },
   {
     title: "Growing the prize pool",
     description:
       "All royalties and a part of the generated yield are added to the treasury, creating a compounding effect and growing future prizes.",
+    icon: <GiLockedChest className="h-[1.8rem] w-[1.8rem]" />,
   },
   {
     title: "One Ticket, Endless Entries",
     description:
       "Purchase your ticket once, and you're set for all future draws. Your ticket remains active indefinitely, giving you a shot at the daily prize every single day without any additional cost.",
+    icon: <MdEventRepeat className="h-[1.8rem] w-[1.8rem]" />,
   },
   {
     title: "Strategic Trading",
     description:
       "Buy and sell tickets in the open market to boost your chances. Spot trends, make smart trades and grow your odds of holding a winning pattern.",
+    icon: <GiCrystalBall className="h-[1.8rem] w-[1.8rem]" />,
   },
   {
     title: "For the community",
     description:
       "UPDWN is built by and for the community. Using onchain governance we will decide how the treasury is invested and explore ways to grow it.",
+    icon: <GiThreeFriends className="h-[1.8rem] w-[1.8rem]" />,
   },
 ];
 
@@ -62,7 +74,10 @@ export function InfoSection() {
           </div>
 
           <div className="flex flex-col justify-center">
-            <h2 className="font-bold text-lg">{item.title}</h2>
+            <div className="flex items-center mb-4">
+              <div className="mr-4">{item?.icon}</div>
+              <h2 className="font-bold text-lg">{item.title}</h2>
+            </div>
             <p className="text-muted-foreground">{item.description}</p>
           </div>
         </div>
